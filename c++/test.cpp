eada6fe7ed7ee481931deecb8d4046f94bc7d30d
KMx404 <mohamedelfaleh5@gmail.com>
2020-10-01 21:02:27 +0100

    :zap: edit - c++ added

---
+++ c++/test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+void hello(){
+	printf("Hello world!"); 
+} 
+
+int main(){
+	int x;
+	x = 1;
+	cout<<x<<"\n";
+	return 0;
+	hello();
+	return 0;
+}
+
+
